c1e113bb7cd65162bf0abbf6795919021252e0e0
12HuYang <yang.hu@wsu.edu>
2019-04-11 00:38:40 -0700

    frozen low layers

--- transfer_crfasrnn.py
+++ transfer_crfasrnn.py
@@ -176,7 +176,8 @@ def main():
     layer_names = [layer.name for layer in model.layers]
     print(layer_names)
     last=layer_names.index('score-fr')
-    for i in range(8+6,last-3):
+    #for i in range(8+6,last-3):
+     for i in range(8,8+6):
         name=layer_names[i]
         c=list(f[name])
         model.layers[i].trainable=False
