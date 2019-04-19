b718122097da87a23e20b7a233a8d38ef79edf95
12HuYang <yang.hu@wsu.edu>
2019-04-19 12:26:22 -0700

    tesla

--- transfer_crfasrnn.py
+++ transfer_crfasrnn.py
@@ -238,7 +238,7 @@ def main():
         print(X_train.shape)
         print(Y_train.shape)
         model=getmodel()
-        model.fit(X_train,Y_train,epochs=2000,batch_size=16)
+        model.fit(X_train,Y_train,epochs=3000,batch_size=16)
         #preds=model.predict(X_test,Y_test)
         #preds=model.evaluate(X_test,Y_test)
         #print ("Loss = " + str(preds[0]))
